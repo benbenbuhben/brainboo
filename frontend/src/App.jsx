ca20a0fe0aa6596d5fc21b63307730075f1fd43d
Benjamin Hurst <benhurst@uw.edu>
2025-03-13 03:27:33 +0000

    Add deployment resources

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,16 @@
-// src/App.jsx
 import React from 'react';
 import { ThemeProvider, CssBaseline, Box } from '@mui/material';
 import theme from './theme';
 import AppRoutes from './routes';
 
 export default function App() {
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Box
         sx={{
           pt: '64px', // Enough to offset the fixed navbar height
-          // or you can do: pt: (theme) => `${theme.mixins.toolbar.minHeight}px`
-          // maxWidth: '70%',
           margin: '0 auto',
           minHeight: '100vh',
         }}
@@ -22,3 +20,4 @@ export default function App() {
     </ThemeProvider>
   );
 }
+
